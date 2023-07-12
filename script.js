f1927c15c5111ddf6ac72b589ad0f489ec7f27f8
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-12 09:55:12 -0300

    trabalhando no menu responsivo

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 600) {
+        itens.style.display = 'block'
+    }
+    else {
+        itens.style.display = 'none'
+    }
+}
